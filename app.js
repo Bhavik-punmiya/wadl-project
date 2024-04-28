297fa36cf9e82a77a0707784a4140b81f891e8f9
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-04-28 16:25:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(express.static('./public'));
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/tasks', tasks);
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
